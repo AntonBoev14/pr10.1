f38f9120d3fa1b61a09fd30b12ebd76a7a2c3997
AntonBoev14 <antonboev421@gmail.com>
2024-11-07 10:09:03 +0300

    Add files via upload

---
+++ pr10.1/pr10.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+double secondsToMinutes(int seconds) {
+    
+    double minutes = seconds / 60.0; 
+    return minutes; 
+}
+
+int main() {
+    setlocale(0, "ru");
+    int seconds;
+    cout << "введите секунды: ";
+    cin >> seconds;
+
+    double result1 = secondsToMinutes(seconds);
+    cout << seconds << " секунд(ы) = " << result1 << " минут(ы)" << endl;
+    cout << "введите секунды: ";
+    cin >> seconds;
+    double result2 = secondsToMinutes(seconds);
+   
+
+    cout << seconds << " секунд(ы) = " << result2 << " минут(ы)" << endl;
+
+    return 0;
+}
